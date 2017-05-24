c79e7b07a3f9ba148e462f1a6e24d4b65d29ce3f
Beheerder Broekhuizen-Versteeg <manZ130194@umcn.nl>
2017-05-25 00:38:01 +0200

    Made game splitter run in manageable time.

--- DataFunctions.R
+++ DataFunctions.R
@@ -1,5 +1,6 @@
 library("plyr")
-
+library(doSNOW)
+library("snow")
 get.supply <- function(game.data, include.std=T){
   if(!exists("card.info")){load("card.info")}
   supply <- regmatches(game.data, gregexpr("S:\\[.*?\\]", game.data))[[1]]
@@ -54,7 +55,7 @@ winner <- function(game.data){
   which.max(get.points.per.player(game.data))
 }
 get.n.players <- function(game.data){
-  return(length(which(strsplit(game.data,"")[[1]]=="*")))
+  return(sum(strsplit(game.data,"")[[1]]=="*"))
 }
 get.buy.orders <- function(game.data){
   player.at <- which(strsplit(game.data,"")[[1]]=="*")
@@ -75,12 +76,12 @@ get.buy.orders <- function(game.data){
   buy.matrix <- array(0, c(max(buy.orders$turn), 173, n.players))
   for(p in 1:n.players){
     if(sum(buy.orders$player == p) > 0)
-    for(t in 1:max(buy.orders$turn[buy.orders$player == p])){
-      my.buys <- as.numeric(strsplit(buy.orders$buys[buy.orders$player == p & buy.orders$turn == t], ",")[[1]])
-      for(b in 1:length(my.buys)){
-        buy.matrix[t, my.buys[b] + 1, p] <- buy.matrix[t, my.buys[b] + 1, p] + 1
+      for(t in 1:max(buy.orders$turn[buy.orders$player == p])){
+        my.buys <- as.numeric(strsplit(buy.orders$buys[buy.orders$player == p & buy.orders$turn == t], ",")[[1]])
+        for(b in 1:length(my.buys)){
+          buy.matrix[t, my.buys[b] + 1, p] <- buy.matrix[t, my.buys[b] + 1, p] + 1
+        }
       }
-    }
   }
   return(list(buy.orders=buy.orders, buy.matrix=buy.matrix))
 }
@@ -91,66 +92,71 @@ game.to.data.row <- function(game){
   # get the supply and the end deck for the winner
   return(tryNULL(get.decks.per.player(tmp[game])[which.max(get.points.per.player(tmp[game], F)), ]))
 }
-get.game.states <- function(game.data){
-  n.players <- get.n.players(game.data)
-  buys <- get.buy.orders(game.data)
-  buy.orders <- buys$buy.orders
-  buy.matrix <- buys$buy.matrix
-  perspective <- winner(game.data)
-  n.turns <- max(buy.orders$turn[buy.orders$player == perspective])
-  last.turns <- rep(0, n.players)
-  for(p in 1:n.players){
-    last.turns[p] <- max(buy.orders$turn[buy.orders$player == p])
-  }
-  game.states <- array(0, dim = c(n.turns, 173, 4)) # current supply, my inv, mean opp inv, my.buys
-  dimnames(game.states) <- list(
-    paste("turn",1:(n.turns)),
-    card.info$Singular,
-    c("supply","my.inventory","opp.inventory","my.buys")
-  )
-  # init the supply at the start of the game
-  # action cards
-  game.states[, get.supply(game.data,F), "supply"] <- 10
-  game.states[, get.std.cards()[c(2,5,6,8)], "supply"] <- rep(100,4)
-  game.states[, get.std.cards()[c(1,3,4,7)], "supply"] <- rep(c(-1,8,12,12)[n.players],4)
-  
-  # walk through the game
-  for(p in 1:n.players){
-    # all players: adjust supply
-    game.states[1:n.turns,,"supply"] <- game.states[,,"supply"] - apply(buy.matrix[1:n.turns,,p], 2, cumsum)
-    # only opponent players: adjust opponent inventory
-    if(p != perspective){
-      game.states[1:n.turns,,"opp.inventory"] <- game.states[1:n.turns,,"opp.inventory"] + apply(buy.matrix[1:n.turns,,p], 2, cumsum)
-    } else {
-      # only perspective player: adjust my inventory
-      game.states[1:n.turns,,"my.inventory"] <- apply(buy.matrix[1:n.turns,,p], 2, cumsum)
-      # only perspective player: denote my buys
-      game.states[1:n.turns,,"my.buys"] <- buy.matrix[1:n.turns,,p]
+get.game.states <- function(game.data, real=F){
+  if(!real){
+    return(try_default(get.game.states(game.data,T),NULL,T))
+  } else{
+    n.players <- get.n.players(game.data)
+    buys <- get.buy.orders(game.data)
+    buy.orders <- buys$buy.orders
+    buy.matrix <- buys$buy.matrix
+    perspective <- winner(game.data)
+    n.turns <- max(buy.orders$turn[buy.orders$player == perspective])
+    last.turns <- rep(0, n.players)
+    for(p in 1:n.players){
+      last.turns[p] <- max(buy.orders$turn[buy.orders$player == p])
     }
+    game.states <- array(0, dim = c(n.turns, 173, 4)) # current supply, my inv, mean opp inv, my.buys
+    dimnames(game.states) <- list(
+      paste("turn",1:(n.turns)),
+      card.info$Singular,
+      c("supply","my.inventory","opp.inventory","my.buys")
+    )
+    # init the supply at the start of the game
+    # action cards
+    game.states[, get.supply(game.data,F), "supply"] <- 10
+    game.states[, get.std.cards()[c(2,5,6,8)], "supply"] <- rep(100,4)
+    game.states[, get.std.cards()[c(1,3,4,7)], "supply"] <- rep(c(-1,8,12,12)[n.players],4)
+    
+    # walk through the game
+    for(p in 1:n.players){
+      # all players: adjust supply
+      game.states[2:n.turns,,"supply"] <- game.states[2:n.turns,,"supply"] - apply(buy.matrix[1:(n.turns - 1),,p], 2, cumsum)
+      # only opponent players: adjust opponent inventory
+      if(p != perspective){
+        game.states[2:n.turns,,"opp.inventory"] <- game.states[2:n.turns,,"opp.inventory"] + apply(buy.matrix[1:(n.turns - 1),,p], 2, cumsum)
+      } else {
+        # only perspective player: adjust my inventory
+        game.states[2:n.turns,,"my.inventory"] <- apply(buy.matrix[1:(n.turns - 1),,p], 2, cumsum)
+        # only perspective player: denote my buys
+        game.states[1:n.turns,,"my.buys"] <- buy.matrix[1:n.turns,,p]
+      }
+    }
+    # transform into matrix
+    game.states <- matrix(game.states, ncol = 692)
+    colnames(game.states) <- paste0(rep(c("s_","d_","o_","b_"),each=173),card.info$Singular)
+    return(game.states)
   }
-  return(game.states)
 }
-get.game.states.inoutput <- function(games){
+get.game.states.inoutput <- function(games, do.parallel=F){
   load("card.info")
-  inputs <- matrix(NA, 1e3, 3 * 173)
-  outputs <- matrix(NA, 1e3, 173)
-  counter <- 1
+  cat("Extracting game states ... ")
   
-  for(i in 1:length(games)){
-    cat("Processing game states for game",i,"...\n")
-    game.states <- get.game.states(games[i])
-    n.turns <- dim(game.states)[1]
-    if(counter + n.turns + 5 > nrow(inputs)){
-      cat("Increasing input/output matrices...\n")
-      inputs <- rbind(inputs, matrix(NA, 1e3, 3 * 173))
-      outputs <- rbind(outputs, matrix(NA, 1e3, 173))
-    }
-    inputs[counter:(counter + n.turns - 1), ] <- cbind(game.states[,,"supply"], game.states[,,"my.inventory"], game.states[,,"opp.inventory"] / (get.n.players(games[i]) - 1))
-    outputs[counter:(counter + n.turns - 1), ] <- game.states[,,"my.inventory"]
-    counter <- counter + n.turns
+  if(do.parallel){
+    cat("in parallel.\n")
+    cl <- makeCluster(8, type = "SOCK")
+    registerDoSNOW(cl)
+    clusterExport(cl,"card.info")
+    clusterCall(cl, source, file="DataFunctions.R")
+    all.states <- llply(.data = games, .fun = get.game.states, .parallel = T, .progress = progress_time())
+    stopCluster(cl)
+  } else {
+    cat("using standard plyr.\n")
+    all.states <- llply(.data = games, .fun = get.game.states, .parallel = F, .progress = progress_time())
   }
-  inputs <- inputs[complete.cases(inputs), ]
-  outputs <- inputs[complete.cases(outputs), ]
   
-  return(list(inputs=inputs, outputs=outputs))
+  cat("Binding game states ...\n")
+  all.states <- do.call(rbind, all.states)
+  colnames(all.states) <- paste0(rep(c("s_", "d_", "o_", "b_"), each = 173), card.info$Singular)
+  return(all.states)
 }
--- user script.R
+++ user script.R
@@ -4,11 +4,21 @@ source("DataFunctions.R")
 source("DeepLearning.R")
 load("card.info")
 
-# load game data
-load("games_1")
-
 # obtain game state data from games
-dat <- get.game.states.inoutput(tmp)
+for(i in 1:160){
+  cat("-------------------",i,"--------------------\n")
+  load(paste0("games_",i))
+  starts <- round(seq(1,length(tmp),length.out = 10))
+  for(s in 2:length(starts)){
+    if(!file.exists(paste0("all_states_",i,"_",letters[s - 1]))){
+      cat("part",paste0("all_states_",i,"_",letters[s - 1]),"\n")
+      all.states <- get.game.states.inoutput(tmp[starts[(s-1)]:starts[s]],do.parallel = T) 
+      save(all.states, file=paste0("all_states_",i,"_",letters[s - 1]))
+      rm(all.states)
+      gc() 
+    }
+  }
+}
 
 # build a nn model and save it to disk
 init.nn(model.name = "nn_model",dat = dat)
