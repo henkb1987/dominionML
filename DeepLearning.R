f9934706b938b91f7118e2bcaaaca5f2d3522352
Beheerder Broekhuizen-Versteeg <manZ130194@umcn.nl>
2017-05-26 09:02:45 +0200

    First version of complete game state learner. I've added one state database to the commit (all_states_1_a). can call load ("all_states_1_a") to get the all.states object in your wd.

--- DeepLearning.R
+++ DeepLearning.R
@@ -1,9 +1,9 @@
 init.nn <- function(model.name, dat){
-  inputs <- dat[[1]]
-  outputs <- dat[[2]]
+  # clean data
+  dat <- dat[complete.cases(dat), ]
   model <- dbn.dnn.train(
-    x = inputs,
-    y = outputs,
+    x = dat[, !grepl("b_", colnames(dat))],
+    y = dat[, grepl("b_", colnames(dat))],
     hidden = c(250),
     learningrate_scale = .9,
     learningrate = .7,
@@ -15,18 +15,18 @@ init.nn <- function(model.name, dat){
   save(model, file=model.name)
 }
 update.nn <- function(model.name, dat){
-  inputs <- dat[[1]]
-  outputs <- dat[[2]]
+  # clean data
+  dat <- dat[complete.cases(dat), ]
   load(model.name)
   model <- nn.train(
-    x = inputs,
-    y = outputs,
+    x = dat[1:100, !grepl("b_", colnames(dat))],
+    y = dat[1:100, grepl("b_", colnames(dat))],
     initW = model$W,
     initB = model$B,
     hidden = c(250),
-    learningrate_scale = .9,
+    learningrate_scale = .8,
     learningrate = .7,
-    numepochs = 1
+    numepochs = 10
   )
   save(model, file=model.name)
 }
--- user script.R
+++ user script.R
@@ -4,26 +4,16 @@ source("DataFunctions.R")
 source("DeepLearning.R")
 load("card.info")
 
-# obtain game state data from games
+# init a nn model and save it to disk
+load("all_states_1_a")
+init.nn(model.name = "nn_model",dat = all.states)
+
+# go through all game state data and update model
 for(i in 1:160){
-  cat("-------------------",i,"--------------------\n")
-  load(paste0("games_",i))
-  starts <- round(seq(1,length(tmp),length.out = 10))
-  for(s in 2:length(starts)){
-    if(!file.exists(paste0("all_states_",i,"_",letters[s - 1]))){
-      cat("part",paste0("all_states_",i,"_",letters[s - 1]),"\n")
-      all.states <- get.game.states.inoutput(tmp[starts[(s-1)]:starts[s]],do.parallel = T) 
-      save(all.states, file=paste0("all_states_",i,"_",letters[s - 1]))
-      rm(all.states)
-      gc() 
-    }
+  for(s in 1:9){
+    load(paste0("all_states_",i,"_",letters[s]))
+    cat("Learning from",paste0("all_states_",i,"_",letters[s]),"oh wow",sum(complete.cases(all.states)),"games to learn from.\n")
+    update.nn("nn_model", all.states)
+    gc()
   }
 }
-
-# build a nn model and save it to disk
-init.nn(model.name = "nn_model",dat = dat)
-
-# load new data and update nn
-load("games_2")
-dat <- get.game.states.inoutput(tmp)
-update.nn("nn_model", dat)
